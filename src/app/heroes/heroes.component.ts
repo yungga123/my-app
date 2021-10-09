2eae43d9fc7d59d74fac9299bb054f279ccb34c0
yungga123 <53594401+yungga123@users.noreply.github.com>
2021-10-09 16:08:44 +0800

    cont hero detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,6 +16,13 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  
+
   constructor() { }
 
   ngOnInit(): void {
